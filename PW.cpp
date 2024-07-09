f4fcb9e919a66c9f958de304f7bd48e450f2e960
Александр Осипов <alikxyzet@gmail.com>
2024-07-09 03:57:25 +0300

    9.1.1 Test framework: Small changes in the code

--- PW.cpp
+++ PW.cpp
@@ -9,76 +9,90 @@ using namespace std;
 
 
 /* ---   Инициализация глобальных переменных   --- */
+
 // Блокиратор для соблюдения очереди вывода данных
 mutex cyclicMutex;
-//---------------------------------------------------
+//---------------------------------------------------------------------
+
+
+
+/* ---   Функции потоков   --- */
 
 // Функция вывода данных
 void LinePrint()
 {
-	// Вывод в консоль с соблюдением очереди
-	cyclicMutex.lock();
-	cout << "LinePrint ID: " << this_thread::get_id() << endl;
-	cyclicMutex.unlock();
+    // Вывод в консоль с соблюдением очереди
+    cyclicMutex.lock();
+    cout << "LinePrint ID: " << this_thread::get_id() << endl;
+    cyclicMutex.unlock();
 }
 
 // Функция сбора и вывода данных
 void DataCollectionAndPrinting(unsigned int time)
 {
-	// Имитация задержки получения данных
-	cout
-		<< "time = " << time
-		<< " ms for ID: " << this_thread::get_id()
-		<< endl;
+    // Имитация задержки получения данных
+    cout
+        << "time = " << time
+        << " ms for ID: " << this_thread::get_id()
+        << endl;
 
-	this_thread::sleep_for(chrono::milliseconds(time));
+    this_thread::sleep_for(chrono::milliseconds(time));
 
-	// Вывод данных
-	LinePrint();
+    // Вывод данных
+    LinePrint();
 }
+//---------------------------------------------------------------------
 
 
 
 int main()
 {
-	/* ---   Предварительное   --- */
-	// Локализация
-	setlocale(LC_ALL, "Russian");
-	// Использовать текущее время в качестве начального значения для генератора псевдослучайных чисел
-	srand(time(0));
-	//----------------------------------------
+    /* ---   Предварительное   --- */
+
+    // Локализация
+    setlocale(LC_ALL, "Russian");
+
+    // Использовать текущее время в качестве начального значения для генератора псевдослучайных чисел
+    srand(time(0));
+    //----------------------------------------
 
 
 
-	/* ---   Инициализация переменных   --- */
-	// Количество потоков
-	const unsigned int numberThreads = 20;
+    /* ---   Инициализация переменных   --- */
 
-	// Массив потоков
-	thread Threads[numberThreads];
+    // Количество потоков
+    const unsigned int numberThreads = 20;
 
-	// Случайное время задержки для каждого потока (мс)
-	unsigned int timeThreads = 0;
-	//----------------------------------------
+    // Массив потоков
+    thread Threads[numberThreads];
 
-
-
-	// Запуск каждого потока
-	for (unsigned int i = 0; i < numberThreads; ++i)
-	{
-		timeThreads = (float)rand() / RAND_MAX * 1000;
-
-		// !!! Передавать "timeThreads" не по ссылке !!!
-		Threads[i] = thread(DataCollectionAndPrinting, timeThreads);
-	}
+    // Случайное время задержки для каждого потока (мс)
+    unsigned int timeThreads = 0;
+    //----------------------------------------
 
 
 
-	// Ожидание окончания каждого потока
-	for (size_t i = 0; i < numberThreads; i++)
-	{
-		Threads[i].join();
-	}
+    // Запуск каждого потока
+    for (unsigned int i = 0; i < numberThreads; ++i)
+    {
+        // Вариант с временем в диапазоне [0, 1000)
+        //timeThreads = (float)rand() / RAND_MAX * 1000;
 
-	return 0;
+        // PS: Используем данный вариант для более показательной
+        // имитации наложения вызова "LinePrint()" в потоках
+        timeThreads = (rand() % 10) * 100;
+
+        // !!! Передавать "timeThreads" не по ссылке !!!
+        Threads[i] = thread(DataCollectionAndPrinting, timeThreads);
+    }
+
+
+
+    // Ожидание окончания каждого потока
+    for (size_t i = 0; i < numberThreads; i++)
+    {
+        Threads[i].join();
+    }
+
+    return 0;
 }
